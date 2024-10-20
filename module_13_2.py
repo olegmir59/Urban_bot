8625099f39f1f2faf4d0ec6956b60da03a2f8c71
olegmir59 <udom1@ya.ru>
2024-10-21 01:36:31 +0700

    Add file

---
+++ module_13_2.py
@@ -0,0 +1,23 @@
+#  Домашнее задание по теме "Хендлеры обработки сообщений".
+# Цель: написать простейшего телеграм-бота, используя асинхронные функции.
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+api = "здесь секретный код"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=["start"])
+async def start_message(message):
+    print("Привет! Я бот помогающий твоему здоровью.")
+
+
+@dp.message_handler()
+async def all_message(message):
+    print("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
