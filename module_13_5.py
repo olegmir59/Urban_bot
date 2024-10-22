fd911cd0b6070cd9d16b9b8a2a8df70da5afb075
olegmir59 <udom1@ya.ru>
2024-10-23 01:05:36 +0700

    Add file

---
+++ module_13_5.py
@@ -0,0 +1,64 @@
+# Домашнее задание по теме "Клавиатура кнопок".
+# Цель: научится создавать клавиатуры и кнопки на них в Telegram-bot.
+#
+# Задача "Меньше текста, больше кликов":
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+api = "здесь секретный код"
+
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton(text="Расчитать")
+button2 = KeyboardButton(text="Информация")
+kb.row(button1, button2)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text=["Расчитать"])
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост (см):')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    await message.answer(f"Ваша норма калорий:{10*int(data['weight'])+6.25*int(data['growth'])+5*int(data['age'])-161}")
+    await state.finish()
+
+@dp.message_handler(commands=["start"])
+async def start_message(message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью. Нажмите кнопку 'Расчитать'", reply_markup=kb)
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
